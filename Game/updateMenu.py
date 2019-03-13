1133de0734e2e88086188d81fb34d7760fd18313
Ibrahim Javed <2265799j@student.gla.ac.uk>
2019-03-13 13:53:28 +0000

    Updated menu code, re-used code now inside a function
    Former-commit-id: 17d15c5a8bb38a5be9c450e8c14cb39f8230b9cc

--- Game/updateMenu.py
+++ Game/updateMenu.py
@@ -4,9 +4,14 @@ import time
 
 class Mixin:
 
+    def update_pointer(self,button,index):
+        self.pointer.center_y = button.center_y
+        self.pointer.center_x = button.center_x - 100
+        self.selected = button
+        self.selected_index = index
+        self.check = 1
+
     def startMenu_update(self):
-
-
         # If centered set boolean to 0
         if abs(self.joystick.y) < DEAD_ZONE:
             self.check = 0
@@ -15,38 +20,23 @@ class Mixin:
         #If joystick pushed up update selected button and pointer position
         if self.joystick.y < 0 and self.check == 0:
             if self.selected == self.about_button:
-                self.pointer.center_y = self.inst_button.center_y
-                self.pointer.center_x = self.inst_button.center_x - 100
-                self.selected = self.inst_button
-                self.selected_index = 1
-                self.check = 1
-                
+                Mixin.update_pointer(self,self.inst_button,1)
 
-
+            
             elif self.selected == self.inst_button:
-                self.pointer.center_y = self.start_button.center_y
-                self.pointer.center_x = self.start_button.center_x - 100
-                self.selected = self.start_button
-                self.selected_index = 0
-                self.check = 1
+                Mixin.update_pointer(self,self.start_button,0)
+
+            
                 
         #Otherwise move pointer down and update selected button
         elif self.check == 0:
             if self.selected == self.start_button:
-                self.pointer.center_y = self.inst_button.center_y
-                self.pointer.center_x = self.inst_button.center_x - 100
-                self.selected = self.inst_button
-                self.selected_index = 1
-                self.check = 1 
+                Mixin.update_pointer(self,self.inst_button,1)
                 
             
             else:
-                self.pointer.center_y = self.about_button.center_y
-                self.pointer.center_x = self.about_button.center_x - 100
-                self.selected = self.about_button
-                self.selected_index = 2
-                self.check = 1
+                Mixin.update_pointer(self,self.about_button,2)
                 
-
+            
 
         self.pointer.update()
